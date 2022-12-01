0c38836335e207da9a6e395ab9e6e9f32298bed4
Desduh <eduardofalandess@gmail.com>
2022-12-01 14:05:53 -0300

    opcao de adicionar varios telefones e rgs

--- front/src/pages/Cadastrar/clientes/index.tsx
+++ front/src/pages/Cadastrar/clientes/index.tsx
@@ -14,6 +14,33 @@ function CadastrarClientes() {
     const [data_rg, setDataRg] = useState('');
     const [telefone, setTelefone] = useState('');
 
+    const [formRgs, setFormRgs] = useState([{ }])
+  
+    let addFormRg = () => {
+        setFormRgs([...formRgs, { rg: rg, data_rg: data_rg }])
+    }
+  
+    let removeFormRg = () => {
+      let newFormValue = [...formRgs];
+      setRg('')
+      setDataRg('')
+      newFormValue.splice(-1);
+      setFormRgs(newFormValue)
+    }
+
+    const [formTelefones, setFormTelefones] = useState([{ }])
+  
+    let addFormTell = () => {
+        setFormTelefones([...formTelefones, { telefone: telefone}])
+    }
+  
+    let removeFormTell = () => {
+      let newFormValue = [...formTelefones];
+      setTelefone('')
+      formTelefones.splice(-1);
+      setFormRgs(formTelefones)
+    }
+
     let clearAreas = () => {
         setNome('')
         setNomeSocial('')
@@ -25,15 +52,16 @@ function CadastrarClientes() {
       }
     function handleSubmit() {
         toast.success('Cadastrado feito com sucesso!');
+        addFormRg()
+        addFormTell()
     
         Axios.post("http://localhost:3001/cadastro/cliente", {
           nome: nome,
           nome_social: nome_social,
           genero: genero,
           cpf: cpf,
-          rg: rg,
-          data_rg: data_rg,
-          telefone: telefone
+          Rgs: formRgs,
+          telefone: formTelefones
         }).then((res)=>{
           console.log(res)
           clearAreas()
@@ -65,18 +93,25 @@ function CadastrarClientes() {
                             <label>CPF:</label>
                             <input type="text" placeholder='XXX.XXX.XXX-XX' onChange={(e) => setCpf(e.target.value)}/>
                         </div>
-                        <div className="field">
-                            <label>RG:</label>
-                            <input type="text" placeholder='XX.XXX.XXX-X' onChange={(e) => setRg(e.target.value)}/>
-                        </div>
-                        <div className="field">
-                            <label>RG data de emissao:</label>
-                            <input type="date" onChange={(e) => setDataRg(e.target.value)}/>
-                        </div>
-                        <div className="field">
-                            <label>Telefone:</label>
-                            <input placeholder='XX XXXXXXXX' type="text" onChange={(e) => setTelefone(e.target.value)}/>
-                        </div>
+                        {formRgs.map((e, index) => (
+                            <div className="field">
+                                <label>RG:</label>
+                                <input type="text" placeholder='XX.XXX.XXX-X' onChange={(e) => setRg(e.target.value)}/>
+                                <br/>
+                                <label>RG data de emissao:</label>
+                                <input type="date" onChange={(e) => setDataRg(e.target.value)}/>
+                            </div>
+                        ))}
+                        <button type="button" className='del' onClick={() => removeFormRg()}>Deletar</button>
+                        <button className="add" type="button" onClick={() => addFormRg()}>Adicionar RG</button>
+                        {formTelefones.map((e, index) => (
+                            <div className="field">
+                                <label>Telefone:</label>
+                                <input placeholder='XX XXXXXXXX' type="text" onChange={(e) => setTelefone(e.target.value)}/>
+                            </div>
+                        ))}
+                        <button type="button" className='del' onClick={() => removeFormTell()}>Deletar</button>
+                        <button className="add" type="button" onClick={() => addFormTell()}>Adicionar Telefone</button>
                         <Button className="submit" variant="outline-dark" type='submit' onClick={() => handleSubmit()}>Cadastrar</Button>{' '}
                     </form>
                 </div>

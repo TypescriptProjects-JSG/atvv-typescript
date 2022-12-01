eeb0e2c9d362766697e7c8ad32dabc4f33c05e96
Desduh <eduardofalandess@gmail.com>
2022-12-01 11:02:41 -0300

    adicao da listagem dos mais consumidos

--- front/src/pages/Listagem/script/maisConsumidos.tsx
+++ front/src/pages/Listagem/script/maisConsumidos.tsx
@@ -2,7 +2,6 @@ import Listagem from "./listagens";
 
 function maisConsiumidos(){
     var dados = Listagem()
-    var clientes = dados[0]
     var produtos = dados[1]
     var servicos = dados[2]
     var relacaoP = dados[3]
@@ -29,8 +28,24 @@ function maisConsiumidos(){
         }
         listaServ.push([servicos[k][1], quantia])
     }
-    
-    return [listaProd, listaServ]
+
+    listaProd = listaProd.sort((a,b) => b[1] - a[1]);
+    listaServ = listaServ.sort((a,b) => b[1] - a[1]);
+
+    var ser = new Array;
+    for(var k=0; listaServ.length>k; ++k){
+        if(listaServ[k][1] != 0){
+            ser.push(listaServ[k])
+        }
+    }
+    var pro = new Array;
+    for(var k=0; listaProd.length>k; ++k){
+        if(listaProd[k][1] != 0){
+            pro.push(listaProd[k])
+        }
+    }
+
+    return [pro, ser]
 }
 
 export default maisConsiumidos;
